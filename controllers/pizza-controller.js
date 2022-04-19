b3549f7766d080d5b5f1d862f3a84497dbe80bfb
Aimee Truong <aimtruong@ucdavis.edu>
2022-04-18 21:44:29 -0700

    add comment controller, model, and routes

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -5,6 +5,12 @@ const pizzaController = {
     // GET all pizzas
     getAllPizza(req, res){
         Pizza.find({})
+            .populate({
+                path: 'comments',
+                select: '-__v'
+            })
+            .select('-__v')
+            .sort({ _id: -1 })
             .then(dbPizzaData => res.json(dbPizzaData))
             .catch(err => {
                 console.log(err);
@@ -15,6 +21,11 @@ const pizzaController = {
     // GET a pizza
     getPizzaById({ params }, res){
         Pizza.findOne({ _id: params.id })
+            .populate({
+                path: 'comments',
+                select: '-__v'
+            })
+            .select('-__v')
             .then(dbPizzaData => {
                 // if no pizza is found
                 if(!dbPizzaData){
